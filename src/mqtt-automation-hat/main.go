def3c45b4adf02be96474f3445b73f05f84aede6
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-20 15:33:46 -0700

    Checkpoint: now receiving MQTT messages on a given topic

---
+++ src/mqtt-automation-hat/main.go
@@ -0,0 +1,55 @@
+// +build go1.11
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/eclipse/paho.mqtt.golang"
+)
+
+var (
+	debug      = flag.Bool("debug", false, "enable verbose logging")
+	brokerHost = flag.String("broker-host", "localhost", "MQTT broker host to use")
+	brokerPort = flag.Int("broker-port", 1883, "MQTT broker port to use")
+	rootTopic  = flag.String("root-topic", "", "Root topic to listen to")
+)
+func main() {
+	flag.Parse()
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	target := fmt.Sprintf("tcp://%s:%d", *brokerHost, *brokerPort)
+	topicFilter := fmt.Sprintf("%s/#", *rootTopic)
+
+	log.Info("connecting to broker: " + target)
+	log.Info("topic filter: " + topicFilter)
+
+	opts := mqtt.NewClientOptions().AddBroker(target).SetClientID("mqtt-automation-hat").SetCleanSession(true)
+
+	c := mqtt.NewClient(opts)
+
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := c.Subscribe(topicFilter, 2, receive); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	// VT: NOTE: Now we wait until we're interrupted
+
+	select {}
+
+	// VT: FIXME: Need to install interrupt handler to shut things down cleanly before leaving
+	// c.Disconnect(250)
+}
+
+func receive(client mqtt.Client, message mqtt.Message) {
+	log.Info(fmt.Sprintf("%s %s", message.Topic(), message.Payload()))
+
+
+}
