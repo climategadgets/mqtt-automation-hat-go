8fd9ca485e6174b07bc3ec11f70c1dffe51e0704
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-20 15:34:14 -0700

    Added HCC message structures

---
+++ src/hcc-shared/messages.go
@@ -0,0 +1,28 @@
+// Package hcc_shared contains data structures that are common for
+// all incoming and outgoing communications between HCC and the outside world
+package hcc_shared
+
+type hcc_message_base struct {
+	Timestamp string `json:"timestamp"`
+	Name string `json:"name"`
+	Signature string `json:"signature"`
+}
+
+type hcc_message_unique struct {
+	Id string `json:"id"`
+}
+
+type hcc_message_response struct {
+	ReplyTo hcc_message_unique `json:"reply-to"`
+}
+
+type hcc_message_sensor struct {
+	hcc_message_base
+	Signal float64 `json:"signal"`
+}
+
+type hcc_message_switch struct {
+	hcc_message_base
+	hcc_message_unique
+	State bool `json:"state"`
+}
