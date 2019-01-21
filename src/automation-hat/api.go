e7590946f8f29c44ac7da28bba07b2cd9e0aca0d
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-21 15:00:56 -0700

    Added Automation HAT abstraction draft (#2)

---
+++ src/automation-hat/api.go
@@ -0,0 +1,53 @@
+// Package automation_hat contains abstractions necessary to control the Pimoroni Automation HAT
+// (https://shop.pimoroni.com/products/automation-hat)
+// Baseline Python code is available at https://github.com/pimoroni/automation-hat
+package automation_hat
+
+type AutomationHAT interface {
+
+	// 3 x 24V @ 2A relays (NC and NO terminals)
+	Relay()  [3]Relay
+
+	// 3 x 12-bit ADC @ 0-24V (±2% accuracy)
+	ADC24()  [3]ADC
+
+	// 3 x 24V tolerant buffered inputs
+	Input()  [3]Input
+
+	// 3 x 24V tolerant sinking outputs
+	Output() [3] Output
+
+	// 1 x 12-bit ADC @ 0-3.3V
+	ADC12()  ADC
+}
+
+type Switch interface {
+	Get() bool
+	Set(bool)
+}
+
+type Relay interface {
+
+	Switch()
+	Light() [2]Light
+}
+
+type ADC interface {
+
+	Light() [3]Light
+}
+
+type Input interface {
+
+	Light() [3]Light
+}
+
+type Output interface {
+
+	Light() [3]Light
+}
+
+type Light interface {
+
+	Switch()
+}
