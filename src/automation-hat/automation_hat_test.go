f04095a0a975effee2f72689a4fc2d9df2cffd5d
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-22 21:32:01 -0700

    Added [failing] test case for lights and relays (#5)

--- src/automation-hat/automation_hat_test.go
+++ src/automation-hat/automation_hat_test.go
@@ -16,3 +16,29 @@ func TestGetAutomationHAT(t *testing.T) {
 	t.Log(fmt.Sprintf("hat1: %d", hat1))
 	t.Log(fmt.Sprintf("hat2: %d", hat2))
 }
+
+// Make sure lights and relays behave as switches and keep state
+func TestRelaySwitch(t *testing.T) {
+
+	hat := GetAutomationHAT()
+
+	testSwitch(t, hat.Relay()[0])
+}
+
+// Make sure lights and relays behave as switches and keep state
+func TestLightSwitch(t *testing.T) {
+
+	hat := GetAutomationHAT()
+
+	testSwitch(t, hat.StatusLights().Power())
+}
+
+func testSwitch(t *testing.T, s Switch) {
+
+	state := s.Get()
+	changed := s.Set(!state)
+
+	if !changed {
+		t.Fatalf(fmt.Sprintf("failed to change state from %t: %s", state, s))
+	}
+}
