756d95e7ec14d613d4ed1bcafffbb3506885b0f3
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-26 00:35:50 -0700

    Now parsing all [currently available] message types

--- src/mqtt-automation-hat/main.go
+++ src/mqtt-automation-hat/main.go
@@ -3,10 +3,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/climategadgets/mqtt-automation-hat-go/src/hcc-shared"
 	"github.com/eclipse/paho.mqtt.golang"
 	log "github.com/sirupsen/logrus"
+	"sync"
 )
 
 var (
@@ -49,6 +52,50 @@ func main() {
 }
 
 func receive(client mqtt.Client, message mqtt.Message) {
-	log.Info(fmt.Sprintf("%s %s", message.Topic(), message.Payload()))
+	log.Debug(fmt.Sprintf("%s %s", message.Topic(), message.Payload()))
 
+	// Let's allow some redundancy and try parsing the payload for several types we know about in parallel,
+	// then discard the ones we don't care about (or the ones that simply didn't parse)
+	var done sync.WaitGroup
+	done.Add(2)
+
+	// Zero or one goroutine will set this to true
+	parsed := false
+
+	go func() {
+		defer done.Done()
+
+		var payload hcc_shared.HccMessageSwitch
+		err := json.Unmarshal(message.Payload(), &payload)
+
+		if err != nil {
+			log.Debug("not a switch: ", err)
+			return
+		}
+
+		log.Info("switch: ", payload)
+		parsed = true
+	}()
+
+	go func() {
+		defer done.Done()
+
+		var payload hcc_shared.HccMessageSensor
+		err := json.Unmarshal(message.Payload(), &payload)
+
+		if err != nil {
+			log.Debug("not a sensor: ", err)
+			return
+		}
+
+		log.Info("sensor: ", payload)
+		parsed = true
+	}()
+
+	done.Wait()
+	log.Debug("all parsers returned")
+
+	if !parsed {
+		log.Warn(fmt.Sprintf("no parsers were able to understand: %s", message.Payload()))
+	}
 }
