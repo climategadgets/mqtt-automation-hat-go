59d2d1538ce5b288d934f0697c419050987afde3
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-01-25 23:26:43 -0700

    Oops... Renamed shared structures to be exportable.

--- src/hcc-shared/messages.go
+++ src/hcc-shared/messages.go
@@ -2,27 +2,27 @@
 // all incoming and outgoing communications between HCC and the outside world
 package hcc_shared
 
-type hcc_message_base struct {
+type HccMessageBase struct {
 	Timestamp string `json:"timestamp"`
 	Name      string `json:"name"`
 	Signature string `json:"signature"`
 }
 
-type hcc_message_unique struct {
+type HccMessageUnique struct {
 	Id string `json:"id"`
 }
 
-type hcc_message_response struct {
-	ReplyTo hcc_message_unique `json:"reply-to"`
+type HccMessageResponse struct {
+	ReplyTo HccMessageUnique `json:"reply-to"`
 }
 
-type hcc_message_sensor struct {
-	hcc_message_base
+type HccMessageSensor struct {
+	HccMessageBase
 	Signal float64 `json:"signal"`
 }
 
-type hcc_message_switch struct {
-	hcc_message_base
-	hcc_message_unique
+type HccMessageSwitch struct {
+	HccMessageBase
+	HccMessageUnique
 	State bool `json:"state"`
 }
