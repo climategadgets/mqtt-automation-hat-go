228eb63c85c41eaa46ed96001d166036debd2946
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-05-09 19:36:31 -0700

    Failing fast on being unable to open rpio (#5)

--- src/automation-hat/automation_hat.go
+++ src/automation-hat/automation_hat.go
@@ -1,6 +1,7 @@
 package automation_hat
 
 import (
+	"fmt"
 	"github.com/stianeikeland/go-rpio"
 	"go.uber.org/zap"
 	"sync"
@@ -92,7 +93,11 @@ func newAutomationHAT() AutomationHAT {
 	// VT: NOTE: We can safely assume that since someone's created an instance,
 	// they're going to use it
 
-	rpio.Open()
+	if err := rpio.Open(); err != nil {
+
+		// VT: NOTE: It makes no sense to continue, just bail out
+		panic(fmt.Sprintf("can't open rpio, reason: %v", err))
+	}
 
 	go func(control <-chan interface{}) {
 
