a8f1d16a0e3e5d87fd770417a0f74f5d19d51db7
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-05-11 16:18:13 -0700

    Corrected LED intensity data type (#4)

--- src/automation-hat/light.go
+++ src/automation-hat/light.go
@@ -6,7 +6,7 @@ type light struct {
 	messageBus
 	pin        uint8
 	state      bool
-	brightness float64
+	brightness byte
 }
 
 type lightCommand struct {
@@ -28,7 +28,7 @@ func (l *light) Set(state bool) bool {
 
 	l.state = state
 
-	zap.S().Infof("light: pin=%v, state=%v, brightness=%v, changed=%v", l.pin, l.state, l.brightness, changed)
+	zap.S().Infof("light: pin=%v, state=%v, brightness=%d, changed=%v", l.pin, l.state, l.brightness, changed)
 
 	// VT: NOTE: Counterintuitively, 'changed' is not always true. Remains to be seen how useful it is, though
 	l.control <- lightCommand{*l, changed}
