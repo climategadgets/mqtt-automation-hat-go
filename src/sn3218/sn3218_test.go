8e688fce585f03269facd7e884b64c93bdb3852e
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-05-11 12:39:23 -0700

    Added a fade test (no gamma correction) (#4)

--- src/sn3218/sn3218_test.go
+++ src/sn3218/sn3218_test.go
@@ -35,3 +35,35 @@ func TestLightRing(t *testing.T) {
 		time.Sleep(250 * time.Millisecond)
 	}
 }
+
+func TestLightFadeSimple(t *testing.T) {
+
+	values := [18]byte{}
+
+	driver := GetSN3218()
+	defer driver.Close()
+
+	driver.Reset()
+	driver.Enable(true)
+
+	// 0b111111111111111111, all of them
+	driver.EnableLEDs(0x3FFFF)
+
+	var intensity byte
+
+	for intensity = 0; intensity < 0xFF; intensity++ {
+
+		for offset := 0; offset < 18; offset++ {
+			values[offset] = intensity
+		}
+		driver.Output(values)
+	}
+
+	for intensity = 0xFF; intensity > 0; intensity-- {
+
+		for offset := 0; offset < 18; offset++ {
+			values[offset] = intensity
+		}
+		driver.Output(values)
+	}
+}
